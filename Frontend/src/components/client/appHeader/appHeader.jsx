08f45ccdd36834b9c8f002c034fc658cd15b25cb
italoburguer <italoburger@gmail.com>
2021-11-17 11:07:25 -0500

    adicion template

--- Frontend/src/components/client/appHeader/appHeader.jsx
+++ Frontend/src/components/client/appHeader/appHeader.jsx
@@ -5,7 +5,9 @@ import "../appHeader/appHeader.css"
 export default function appHeader(){
     return(
         <header>
-            <h1> ESTA ES LA CABECERA DE NAVEGACIÓN</h1>
+{/* 
+           <h1> ESTA ES LA CABECERA DE NAVEGACIÓN</h1> 
+            */}
         </header>
     );
 }

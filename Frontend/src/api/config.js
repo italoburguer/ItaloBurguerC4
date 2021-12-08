c6d55a9fed0aa9940819729d2496977c5fc0fa01
Fernando Fernandez <fermen1305@gmail.com>
2021-12-08 11:12:58 -0500

    Heroku deploy

--- Frontend/src/api/config.js
+++ Frontend/src/api/config.js
@@ -1,2 +1,2 @@
-export const basePath = "http://localhost:3080/api";
-export const apiVersion = "IB";
+export const basePath = "https://italoburguerserver.herokuapp.com/api";
+export const apiVersion = "ib";
